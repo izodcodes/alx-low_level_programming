0227e5f55bd2c19eb1256ae6c79f40f523b6eb8e
Izod <joshuaokundaye@gmail.com>
2021-08-26 09:19:44 -0700

    Puts test

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alx[100];
+
+	strcpy(alx, "Programming is like building a multilingual puzzle, \n");
+
+	puts(alx);
+	return (0);
+}
