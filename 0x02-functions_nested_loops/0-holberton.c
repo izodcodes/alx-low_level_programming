0d7bb90ed90629081b4ec4c84d4d55396a620d96
Izod <joshuaokundaye@gmail.com>
2021-08-31 05:06:05 -0700

    putchar

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,13 +1,13 @@
 #include "main.h"
-
 /**
  * main - Holberton
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	char *c = "Holberton";
+	char *c = "_putchar";
 	int i;
 	for (i = 0; i < 10; i++)
 	{
