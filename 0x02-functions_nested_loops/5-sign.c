5358738d44c027ebd9f55df590dbe4f955656c5d
Izod <joshuaokundaye@gmail.com>
2021-08-31 11:02:52 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign- prints the sign of a number
+ *@n: random integer
+ * Return: 1 greater than 0, 0 is zero, -1 less than zero
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar (43);
+		return (1);
+	}
+	else if (n = 0)
+	{
+		_putchar (48);
+		return (0);
+	}
+	else
+	{
+		_putchar (45);
+		return (-1);
+	}
+}
