5157845dbec24fd26a06bd103cae45fc7a4a8f87
Izod <joshuaokundaye@gmail.com>
2021-08-31 05:32:29 -0700

    Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - Print alphabets
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void);
+int main(void)
+{
+    print_alphabet();
+    return (0);
+}
+void print_alphabet(void)
+{
+	char *c = 'a';
+	while (*c <= 'z')
+	{
+		_putchar(*c++);
+	}
+	_putchar ('\n');
+}
