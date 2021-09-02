477ec91660767b348b7e660c074bd1cf3cc5b529
Izod <joshuaokundaye@gmail.com>
2021-09-02 16:41:46 -0700

    squares

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle
+ *@size: size of the triangle
+ * Return: Always 0 (Success)
+ */
+
+void print_triangle (int size)
+{
+	int s = size;
+	int l;
+	int b;
+
+	if (s > 0)
+	{
+		for (l = 1; l <= s; l++)
+		{
+			for (b = l; b <= s; b++)
+			{
+				_putchar(' ');
+			}
+
+			for (b = 1; b <= l; b++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
