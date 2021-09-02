03e01d852003d2f541a6a25b267c992b2ebab774
Izod <joshuaokundaye@gmail.com>
2021-09-02 10:34:00 -0700

    mul

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -4,14 +4,10 @@
  * mul - multiplies two integer
  *@a: integer
  *@b: integer
- *@c: intger
  * Return: Always 0 (Success)
  */
 
 int mul(int a, int b)
 {
-	int c;
-
-	c = a * b;
-	_putchar(c);
+	retutn (a * b);
 }
