e9662f96a9360730bc9708606d40d4b8fa810edf
Izod <joshuaokundaye@gmail.com>
2021-09-06 11:11:41 -0700

    prints half

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,14 +11,14 @@ void puts2(char *str)
 
 	while (count >= 0)
 	{
+		if (str[count] % 2 == 0)
+		{
+			_putchar(str[count]);
+		}
 		if (str[count] == '\0')
 		{
 			_putchar('\n');
 			break;
-		}
-		if (str[count] % 2 == 0)
-		{
-			_putchar(str[count]);
 		}
 		count++;
 
