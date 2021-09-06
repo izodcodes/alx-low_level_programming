4b695a2e0e60ee14be981b23b7a3de6d9bdbbece
Izod <joshuaokundaye@gmail.com>
2021-09-06 11:16:14 -0700

    every other

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,20 +7,12 @@
  */
 void puts2(char *str)
 {
-	int count = 0;
+	int i = 0;
 
-	while (count >= 0)
+	while (*(str + i))
 	{
-		if (str[count] % 2 == 0)
-		{
-			_putchar(str[count]);
-		}
-		if (str[count] == '\0')
-		{
-			_putchar('\n');
-			break;
-		}
-		count++;
-
+		_putchar(*(str + i));
+		i = i + 2;
 	}
+	_putchar('\n');
 }
