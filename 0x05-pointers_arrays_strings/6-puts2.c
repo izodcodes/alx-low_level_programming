f1071701985a3fb081b79b924bf17fd46cdcb170
Izod <joshuaokundaye@gmail.com>
2021-09-06 11:59:02 -0700

    print half

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,18 +1,35 @@
 #include "main.h"
 
 /**
- * puts2 - prints out every other character of a string
- * @str: a string
- * Return: null
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
  */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
+}
+
+/**
+ * puts2 - prints every second character of a string, followed by a new line
+ * @str: string to print
+ */
+
 void puts2(char *str)
 {
-	int i = 0;
+	int i;
 
-	while (*(str + i))
-	{
-		_putchar(*(str + i));
-		i = i + 2;
-	}
+	for (i = 0; str[i] != '\0' && i < _strlen(str); i += 2)
+		_putchar(str[i]);
+
 	_putchar('\n');
 }
