922c2f7b3d0a5c273e5d2d660256c59866f8ff7d
Izod <joshuaokundaye@gmail.com>
2021-09-06 04:22:28 -0700

    Swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates value
+ * @a: pointer
+ * @b: pointer
+ * Return: Always 0 (Success)
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = b;
+	*b = a;
+}
