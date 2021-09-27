9ad38efb838e227e8dff7507f2b3b9b0cbf8f1a4
Izod <joshuaokundaye@gmail.com>
2021-09-27 12:39:05 -0700

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H_
+#define DOG_H_
+
+/**
+ * struct dog - information about dogs
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+
+#endif /* DOG_H_ */
