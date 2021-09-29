ac54b9657d90dfdbc393a6206f94b549b5cad511
Izod <joshuaokundaye@gmail.com>
2021-09-29 14:33:06 -0700

    Whats my name

---
+++ 0x0F-function_pointers/0-print-name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - execute a function given as a parameter
+ * @name: parameter that function ptr needs
+ * @f:function to execute
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
